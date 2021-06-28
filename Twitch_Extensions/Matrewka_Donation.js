f39d127aacc8ba632b3c2eefca5a47431f497298
WqR128 <wqrwin128@gmail.com>
2021-06-28 18:07:55 +0300

    Update Matrewka_Donation.js

--- Twitch_Extensions/Matrewka_Donation.js
+++ Twitch_Extensions/Matrewka_Donation.js
@@ -1,15 +1,4 @@
 //Additional functionality 
-//Yandex.Metrika
-   (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
-   m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
-   (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
-
-   ym(50856489, "init", {
-        clickmap:true,
-        trackLinks:true,
-        accurateTrackBounce:true
-   });
-document.body.innerHTML +=`<noscript><div><img src="https://mc.yandex.ru/watch/50856489" style="position:absolute; left:-9999px;" alt="" /></div></noscript>`;
 
 
 (function(i, s, o, g, r, a, m) {
