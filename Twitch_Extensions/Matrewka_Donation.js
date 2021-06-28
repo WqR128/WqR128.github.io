4866891cd833c304dc47ffa1cbb3724f5ae17ab7
WqR128 <wqrwin128@gmail.com>
2021-06-28 18:06:12 +0300

    Update Matrewka_Donation.js

--- Twitch_Extensions/Matrewka_Donation.js
+++ Twitch_Extensions/Matrewka_Donation.js
@@ -10,3 +10,22 @@
         accurateTrackBounce:true
    });
 document.body.innerHTML +=`<noscript><div><img src="https://mc.yandex.ru/watch/50856489" style="position:absolute; left:-9999px;" alt="" /></div></noscript>`;
+
+
+(function(i, s, o, g, r, a, m) {
+    i['GoogleAnalyticsObject'] = r;
+    i[r] = i[r] || function() {
+        (i[r].q = i[r].q || []).push(arguments)
+    }, i[r].l = 1 * new Date();
+    a = s.createElement(o), m = s.getElementsByTagName(o)[0];
+    a.async = 1;
+    a.src = g;
+    m.parentNode.insertBefore(a, m)
+})(window, document, 'script', 'https://www.googletagmanager.com/gtag/js?id=UA-173470587-1, 'ga');
+
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-173470587-1');
+
